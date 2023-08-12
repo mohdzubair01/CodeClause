d7b779f06b6b1eab8e7afa04393ab398d24ff2e4
Mohd Zubair <141275318+mohdzubair01@users.noreply.github.com>
2023-08-12 16:29:46 +0530

    Add files via upload

---
+++ calculater_in_python.py
@@ -0,0 +1,50 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Cannot divide by zero"
+    return x / y
+
+def calculator():
+    print("Welcome to the Simple Calculator!")
+    print("Operations:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    while True:
+        choice = input("Enter your choice (1/2/3/4/5): ")
+
+        if choice == '5':
+            print("Exiting the calculator. Goodbye!")
+            break
+
+        if choice not in ('1', '2', '3', '4'):
+            print("Invalid choice. Please try again.")
+            continue
+
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+
+        if choice == '1':
+            result = add(num1, num2)
+        elif choice == '2':
+            result = subtract(num1, num2)
+        elif choice == '3':
+            result = multiply(num1, num2)
+        elif choice == '4':
+            result = divide(num1, num2)
+
+        print("Result: ", result)
+
+calculator()
+
